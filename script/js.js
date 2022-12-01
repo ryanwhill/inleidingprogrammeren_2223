a9f47587e5a989385d4aa3e73cafa355a4c69912
ryanwhill <ryan.hillert@hva.nl>
2022-12-01 18:46:07 +0100

    niet op letten
    het programma deed ff raar dus je hoeft niet op deze commit te letten!

--- script/js.js
+++ script/js.js
@@ -6,22 +6,9 @@ var playKnop = document.querySelector('.playbutton');
 
 var playbarElement = document.querySelector('.playbar')
 
-var random = Math.random()* playbarImages.length;
+var random = Math.random() * playbarImages.length;
 
-function logo() {
-console.log("hoi")
-// playbarElement.src = '/images/'
-console.log(playbarElement.src)
-
-// http://127.0.0.1:5501/images/
-// playbarElement.src = 
-
-playbarElement.src = "http://127.0.0.1:5501/images/" + playbarImages[random];
-}
-
-playKnop.addEventListener("click", logo, false);
-
-
+function logo()
 
 
 
