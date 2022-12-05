29c4fd4fd867223079ce85850ed540c10aa7cfc3
ryanwhill <ryan.hillert@hva.nl>
2022-12-05 12:32:16 +0100

    05-12-2022
    functie toegevoegd voor de feedbar. In de les even kijken hoe het werkt met meerdere images.

--- script/js.js
+++ script/js.js
@@ -2,13 +2,17 @@ console.log("dit werkt gewoon, klaar")
 
 var playbarImages = ["playbar01.png", "playbar02.png", "playbar03.png", "playbarfull.png"];
 
+var feedbarImages = ["feedbar01.png", "feedbar02.png", "feedbar03", "feedbarfull.png"]
+
 var playKnop = document.querySelector('.playbutton');
 
-var petKnop = document.querySelector('.petbutton');
+var feedKnop = document.querySelector('.feedbutton');
 
 var sleepKnop = document.querySelector('.sleepbutton')
 
 var playbarElement = document.querySelector('.playbar');
+
+var feedbarElement = document.querySelector('.feedbar');
 
 var Mudkip = document.querySelector(".mudkip01");
 
@@ -23,4 +27,7 @@ function mudkipGoesToSleep() {
 };
 sleepKnop.addEventListener("click", mudkipGoesToSleep)
 
-
+function feedbarIncrease() {
+    feedbarElement.src = "./images/feedbar01.png"
+};
+feedKnop.addEventListener("click", feedbarIncrease);
