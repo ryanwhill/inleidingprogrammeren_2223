0dbef7fdcf7199aed61866fe7ba2397d2d784d3b
ryanwhill <ryan.hillert@hva.nl>
2022-12-06 11:49:42 +0100

    06-12-2022
    bedoeling is dat de playbar en feedbar bijhouden hoevaak er geklikt wordt. Als beide bars vol zijn moet je op het evolve knopje kunnen drukken waardoor Mudkip verandert in Marshtomp. Alleen gebeurt dat nu niet met de huidige code helaas

--- script/js.js
+++ script/js.js
@@ -8,7 +8,9 @@ var playKnop = document.querySelector('.playbutton');
 
 var feedKnop = document.querySelector('.feedbutton');
 
-var sleepKnop = document.querySelector('.sleepbutton')
+var sleepKnop = document.querySelector('.sleepbutton');
+
+var evolveKnop = document.querySelector('.evolvebutton');
 
 var playbarElement = document.querySelector('.playbar');
 
@@ -16,18 +18,39 @@ var feedbarElement = document.querySelector('.feedbar');
 
 var Mudkip = document.querySelector(".mudkip01");
 
+var playCount = 0;
+
+var feedCount = 0;
+
 
 function playbarIncrease() {
+    playKnop.addEventListener('click', function Evolve() {
+        if (playCount < 3) {
+            playCount++;
+            if (playCount == 3) {
+                Mudkip.src = "./images/marshtomp_evolve.png";
+            }
+        }
+    })
     playbarElement.src = "./images/playbar01.png"
 };
-playKnop.addEventListener("click", playbarIncrease)
+
 
 function mudkipGoesToSleep() {
     Mudkip.src = "./images/mudkipsleep.png"
 };
-sleepKnop.addEventListener("click", mudkipGoesToSleep)
+sleepKnop.addEventListener("click", mudkipGoesToSleep);
+
 
 function feedbarIncrease() {
     feedbarElement.src = "./images/feedbar01.png"
 };
 feedKnop.addEventListener("click", feedbarIncrease);
+
+
+function Evolve() {
+    Mudkip.src = "./images/marshtomp_evolve.png"
+}
+evolveKnop.addEventListener("click", Evolve) 
+
+console.log(playCount)
