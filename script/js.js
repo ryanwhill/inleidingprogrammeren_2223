6e7f258dab28514fcc0b3edb2347eb4b2ef62057
ryanwhill <ryan.hillert@hva.nl>
2022-12-08 16:01:46 +0100

    08-12-2022
    Javscript toegevoegd

--- script/js.js
+++ script/js.js
@@ -1,56 +1,49 @@
-console.log("dit werkt gewoon, klaar")
-
+// array declaration
 var playbarImages = ["playbar01.png", "playbar02.png", "playbar03.png", "playbarfull.png"];
-
-var feedbarImages = ["feedbar01.png", "feedbar02.png", "feedbar03", "feedbarfull.png"]
-
+var feedbarImages = ["feedbar01.png", "feedbar02.png", "feedbar03.png", "feedbarfull.png"]
+// interaction element declaration
 var playKnop = document.querySelector('.playbutton');
-
 var feedKnop = document.querySelector('.feedbutton');
-
 var sleepKnop = document.querySelector('.sleepbutton');
-
 var evolveKnop = document.querySelector('.evolvebutton');
-
 var playbarElement = document.querySelector('.playbar');
-
 var feedbarElement = document.querySelector('.feedbar');
-
 var Mudkip = document.querySelector(".mudkip01");
-
+// count declaration
 var playCount = 0;
-
 var feedCount = 0;
+var evoleState = false;
 
+// function declarations
+// event listeners
+playKnop.addEventListener('click', function() {
+    // play button logic
+    if(playCount < 3) {
+        playCount ++;
+        playbarElement.src = './images/' + playbarImages[playCount];
+    }
+});
 
-function playbarIncrease() {
-    playKnop.addEventListener('click', function Evolve() {
-        if (playCount < 3) {
-            playCount++;
-            if (playCount == 3) {
-                Mudkip.src = "./images/marshtomp_evolve.png";
-            }
-        }
-    })
-    playbarElement.src = "./images/playbar01.png"
-};
+feedKnop.addEventListener('click', function() {
+    // feed button logic
+    if(feedCount < 3) {
+        feedCount ++;
+        feedbarElement.src = './images/' + feedbarImages[feedCount];
+    }
+});
 
-
-function mudkipGoesToSleep() {
+sleepKnop.addEventListener('click', function() {
     Mudkip.src = "./images/mudkipsleep.png"
-};
-sleepKnop.addEventListener("click", mudkipGoesToSleep);
+});
 
 
-function feedbarIncrease() {
-    feedbarElement.src = "./images/feedbar01.png"
-};
-feedKnop.addEventListener("click", feedbarIncrease);
-
-
-function Evolve() {
-    Mudkip.src = "./images/marshtomp_evolve.png"
-}
-evolveKnop.addEventListener("click", Evolve) 
-
-console.log(playCount)
+evolveKnop.addEventListener('click', function() {
+    // evolve button logic
+    if (playCount === 3 && feedCount === 3) {
+        // logic for evolve pokemon
+        Mudkip.src = "./images/marshtomp_evolve.png"
+    } else {
+        // return because we dont want to do anything
+        return;
+    }
+});
